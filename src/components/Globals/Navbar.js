8cc5183654fd8f00ffaa04104ceb1b806b0e32a3
bdmllc <beachdaymedia@gmail.com>
2019-05-13 15:10:32 -0400

    added Navbar and Navbar Logo

--- src/components/Globals/Navbar.js
+++ src/components/Globals/Navbar.js
@@ -17,10 +17,22 @@ export default class Navbar extends Component {
         path: "/about/",
         text: "about",
       },
+      {
+        id: 3,
+        path: "/contact/",
+        text: "contact",
+      },
     ],
   }
 
-  navbarHandler = () => {}
+  navbarHandler = () => {
+    this.state.navbarOpen
+      ? this.setState({ navbarOpen: false, css: "collapse navbar-collapse" })
+      : this.setState({
+          navbarOpen: true,
+          css: "collapse navbar-collapse show",
+        })
+  }
 
   render() {
     return (
@@ -28,6 +40,26 @@ export default class Navbar extends Component {
         <Link to="/" className="navbar-brand">
           <img src={logo} alt="logo" />
         </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={this.navbarHandler}
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className={this.state.css}>
+          <ul className="navbar-nav mx-auto">
+            {this.state.links.map(link => {
+              return (
+                <li key={link.id} className="nav-item">
+                  <Link to={link.path} className="nav-link text-capitalize">
+                    {link.text}
+                  </Link>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
       </nav>
     )
   }
